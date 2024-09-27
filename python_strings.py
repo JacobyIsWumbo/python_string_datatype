a367efd2df51b1ffcab46775cb6d2bc41acfb42b
Jacob Christensen <jacobiswumbo@gmail.com>
2024-09-26 18:32:29 -0600

    escape char and formatting strings

--- python_strings.py
+++ python_strings.py
@@ -8,7 +8,10 @@
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
+my_first_name = 'Jacob'
+my_last_name = 'Christensen'
+my_year_of_birth = 2005
+current_year = 2020
 
 
 
@@ -20,7 +23,12 @@
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
+print(my_first_name)
+print(my_last_name)
+print(my_first_name[0])
+print(my_last_name[-4])
+print(my_first_name[:2])
+print(my_last_name[-4:-6])
 
 
 
@@ -28,6 +36,8 @@
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
+print(my_first_name + my_last_name)
+print(my_first_name*6)
 
 
 
@@ -37,13 +47,18 @@
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
+print(f'{my_first_name} was born in {my_year_of_birth}')
+print(f'{my_first_name} was born in {my_year_of_birth}. {my_first_name} enjoyed celebrating {current_year}')
 
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
+print(my_first_name + '"s birth year is ' + str(my_year_of_birth))
+print(my_first_name + '\'s birth year is ' + str(my_year_of_birth))
+print ('\n', my_last_name, current_year)
+
 
 
 # TODO String methods
